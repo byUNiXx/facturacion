245f66f6cdeed67d8be50c216538dc87e794dc8e
byUNiXx <clmjuanluisc@gmail.com>
2017-02-21 14:02:45 +0100

    Update cliente.java

--- cliente.java
+++ cliente.java
@@ -1,1 +1,31 @@
+package cliente;
 
+public class cliente {
+	
+	private String NIF;
+	private String nombre;
+	private Direccion direccion;
+	private String mail;
+	private String fecha;
+	private Tarifa tarifa;
+	
+	//no comun
+	private String apellidos;
+	
+
+	public cliente(){
+	}
+	
+	public cliente(String NIF, String nombre, Direccion direccion, String mail,
+			String fecha, Tarifa tarifa){
+		
+		this.NIF
+		
+	}
+	public 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
