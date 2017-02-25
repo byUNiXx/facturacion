3350a615d9d8def71618103dc2262d5f6236985f
byUNiXx <clmjuanluisc@gmail.com>
2017-02-25 18:12:22 +0100

    Update Cliente.java

--- clientes/Cliente.java
+++ clientes/Cliente.java
@@ -1,20 +1,26 @@
 package cliente;
 
+import fecha.Fecha;
+
 public class Cliente{
 	
     private String NIF;
     private String nombre;
     private String correoElectronico;
     private Direccion direccion;
+    private Fecha fechaAlta;
+    private Tarifa tarifa; //Falta clase tarifa por definir
     
     public Cliente(){
     }
     
-    public Cliente(String NIF, String nombre, String correoElectronico, Direccion direccion) {
+    public Cliente(String NIF, String nombre, String correoElectronico, Direccion direccion, Fecha fecha, Tarifa tarifa) {
         this.NIF = NIF;
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;
         this.direccion = direccion;
+        this.fechaAlta = fecha;
+        this.tarifa = tarifa;
     }
     
     public Direccion getDireccion() {
@@ -33,10 +39,18 @@ public class Cliente{
         return correoElectronico;
     }
     
+    public String getFechaAlta() {
+        return fechaAlta.toString();
+    }
+    
+    public String getTarifa() { 
+        return tarifa.toString();
+    }
+    
     public static void main(String[] args) {
         
-    	Particular cliente = new Particular("53788220Y","Diego","Cano","al342376@uji.es",new Direccion("12540","Vila-real"
+    	Particular cliente = /*new Particular("53788220Y","Diego","Cano","al342376@uji.es",new Direccion("12540","Vila-real"
                 ,"Castellón"));
-        System.out.println(cliente.getCorreoElectronico());
+        System.out.println(cliente.getCorreoElectronico());*/
     }
 }
