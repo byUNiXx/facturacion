eb5f43efb4f1cb6090a5696f8700ab231b643eed
byUNiXx <clmjuanluisc@gmail.com>
2017-02-25 22:24:57 +0100

    Update Particular.java

--- clientes/Particular.java
+++ clientes/Particular.java
@@ -1,18 +1,21 @@
 package cliente;
 
 import fecha.Fecha;
+import tarifa.Tarifa;
 
 public class Particular extends Cliente{
 	
 	private String apellidos;
 	
-	public Particular(String NIF, String nombre, String correoElectronico, Direccion direccion, Fecha fecha, Tarifa tarifa, String apellidos){
+	public Particular(String NIF, String nombre, String apellidos,
+			String correoElectronico, Direccion direccion, Fecha fecha, Tarifa tarifa){
+		
 		super(NIF, nombre, correoElectronico, direccion, fecha, tarifa);
 		
 		this.apellidos = apellidos;
 	}
 	
 	public String getApellidos(){
-		return this.apellidos;
+		return apellidos;
 	}
 }
