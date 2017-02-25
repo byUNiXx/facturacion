371eccba703e94ff0974945892c6627802fbac2b
byUNiXx <clmjuanluisc@gmail.com>
2017-02-25 17:50:41 +0100

    Update Particular.java

--- clientes/Particular.java
+++ clientes/Particular.java
@@ -1,1 +1,16 @@
+package cliente;
 
+public class Particular extends Cliente{
+	
+	private String apellidos;
+	
+	public Particular(String NIF, String nombre, String apellidos, String correoElectronico, Direccion direccion){
+		super(NIF, nombre, correoElectronico, direccion);
+		
+		this.apellidos = apellidos;
+	}
+	
+	public String getApellidos(){
+		return this.apellidos;
+	}
+}
