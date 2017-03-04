8a6220187ea13d91f3f804f16dcbd6ec985adfa1
byUNiXx <clmjuanluisc@gmail.com>
2017-03-04 15:54:50 +0100

    Update Cliente.java

--- clientes/Cliente.java
+++ clientes/Cliente.java
@@ -1,6 +1,9 @@
 package cliente;
 
-import fecha.Fecha;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 import tarifa.Tarifa;
 
 public class Cliente{
@@ -9,13 +12,14 @@ public class Cliente{
     private String nombre;
     private String correoElectronico;
     private Direccion direccion;
-    private Fecha fechaAlta;
-    private Tarifa tarifa; 
+    private LocalDate fechaAlta;
+    private Tarifa tarifa;
     
     public Cliente(){
     }
-    
-    public Cliente(String NIF, String nombre, String correoElectronico, Direccion direccion, Fecha fecha, Tarifa tarifa) {
+
+    public Cliente(String NIF, String nombre, String correoElectronico, Direccion direccion, 
+    		LocalDate fecha, Tarifa tarifa) {
         this.NIF = NIF;
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;
@@ -40,19 +44,37 @@ public class Cliente{
         return correoElectronico;
     }
     
-    public String getFecha() {
-        return fechaAlta.toString();
+    public LocalDate getFecha() {
+        return fechaAlta;
     }
     
-    public String getTarifa() { 
-        return tarifa.toString();
+    public Tarifa getTarifa() { 
+        return tarifa;
+    }
+    
+    public void cambiarTarifa(Tarifa nueva){
+    	tarifa = nueva;
+    }
+    
+    public String toString(){
+    	
+    	return NIF + "  " +  nombre + "  " + correoElectronico + "  " + direccion 
+    			+ "  " + fechaAlta + "  " + tarifa;
     }
     
     public static void main(String[] args) {
+    	
+    	LocalDate fecha = LocalDate.of(2017, 11, 23);
+    	
+    	LocalTime hora = LocalTime.now();
+    	
+    	Cliente cliente = new Particular("53788220Y","Diego","Cano","al342376@uji.es",
+    			new Direccion("12540","Vila-real","Castellón"),fecha , new Tarifa(0.37));
+    	
+    	/*Cliente cliente2 = new Empresa("53788220Y","Diego","al342376@uji.es",
+    			new Direccion("12540","Vila-real","Castellón"), new Fecha(25, 01, 2017), new Tarifa(0.37));*/
         
-    	Particular cliente = new Particular("53788220Y","Diego","Cano","al342376@uji.es",
-    			new Direccion("12540","Vila-real","Castellón"), new Fecha(25, 01, 2017), new Tarifa(0.37));
-        
-    	System.out.println(cliente.getTarifa().toString());
+    	System.out.println(cliente);
+    	System.out.println(hora.truncatedTo(ChronoUnit.SECONDS));
     }
 }
