5ae37663c4a53ec6e9918c35635062ea00d0aca0
byUNiXx <clmjuanluisc@gmail.com>
2017-03-06 10:54:21 +0100

    Update DatosLlamadas.java

--- datos/DatosLlamadas.java
+++ datos/DatosLlamadas.java
@@ -1,5 +1,7 @@
 package datos;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +18,11 @@ public class DatosLlamadas{
 		data = new HashMap<>();
 	}
 	
+	public Map<Cliente, ArrayList<Llamada>> getData(){
+		
+		return data;
+	}
+	
 	public void añadirLlamada(Cliente cliente, Llamada llamada){
 		
 		if(!data.containsKey(cliente)){
@@ -28,11 +35,23 @@ public class DatosLlamadas{
 		
 		return data.get(cliente);
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public ArrayList<LocalTime> buscarLlamadaPeriodo 
+	(Cliente cliente, LocalDateTime periodoInicio, LocalDateTime periodoFinal){
 		
+		ArrayList<Llamada> llamadasCliente = llamadasCliente(cliente);
+		ArrayList<LocalTime> llamadasRango = new ArrayList<>();
+		
+		Llamada aux;
+		
+		for(int i = 0; i < llamadasCliente.size(); i++){
+			
+			aux = llamadasCliente.get(i);
+			
+			if(aux.getFecha().compareTo(periodoInicio) >= 0 && aux.getFecha().compareTo(periodoFinal) <= 0)
+				llamadasRango.add(aux.getDuracion());
+		}
+		
+		return llamadasRango;
 	}
-
 }
