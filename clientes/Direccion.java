8e4cd9ae5978b87ea063504862987afcca3908ba
byUNiXx <clmjuanluisc@gmail.com>
2017-03-24 13:09:33 +0100

    Update Direccion.java

--- clientes/Direccion.java
+++ clientes/Direccion.java
@@ -1,21 +1,29 @@
 package cliente;
 
-public class Direccion {
-	
-	private String cod;
-	private String prov;
-	private String pob;
+import java.time.LocalDate;
 
-	public Direccion(String cod, String pob, String prov){
-		this.cod=cod;
-		this.prov=prov;
-		this.pob=pob;
+import tarifa.Tarifa;
+
+public class Particular extends Cliente{
+	
+	private String apellidos;
+	
+	public Particular(String NIF, String nombre, String apellidos,
+			String correoElectronico, Direccion direccion, LocalDate fecha, Tarifa tarifa){
+		
+		super(NIF, nombre, correoElectronico, direccion, fecha, tarifa);
+		
+		this.apellidos = apellidos;
+	}
+	
+	public String getApellidos(){
+		return apellidos;
 	}
 	
 	public String toString(){
 		
-		return this.cod + " " +  this.pob + " " + this.prov;
-		
+		return getNIF() + "  " +  getNombre() + "  " + getApellidos() + "  " 
+				+getCorreoElectronico() + "  " + getDireccion() + "  " + getFecha() 
+				+ "  " + getTarifa();
 	}
 }
-
