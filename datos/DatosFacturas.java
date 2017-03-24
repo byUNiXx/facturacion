fc43bc56a5965bc37f92ec921297431a29ac979b
byUNiXx <clmjuanluisc@gmail.com>
2017-03-24 16:56:05 +0100

    Update DatosFacturas.java

--- datos/DatosFacturas.java
+++ datos/DatosFacturas.java
@@ -1,109 +1,37 @@
-package factura;
+package datos;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import cliente.Cliente;
-import datos.DatosLlamadas;
-import tarifa.Tarifa;
+import factura.Factura;
 
-public class Factura {
+public class DatosFacturas{
 	
-	 private int codigo;
-	 private Tarifa tarifa;
-	 private LocalDate fechaEmision;
-	 private LocalDateTime periodoInicio;
-	 private LocalDateTime periodoFinal;
-	 private double importe;
+	private Map<Cliente, ArrayList<Factura>> data;
 	
-	public Factura(int codigo, Tarifa tarifa, LocalDate fechaEmision,
-		LocalDateTime periodoInicio, LocalDateTime periodoFinal, double importe)
-		throws IllegalArgumentException{
+	public DatosFacturas(){
 		
-		if(periodoInicio.compareTo(periodoFinal) >= 0)
-			throw new IllegalArgumentException("La factura tiene un periodo de facturacion invalido. "
-					+ "La fecha de comienzo del periodo no puede ser mayor o igual a la del fin.");
-		
-		if(fechaEmision.compareTo(periodoFinal.toLocalDate()) < 0)
-			throw new IllegalArgumentException("La factura tiene una fecha de emision invalida. "
-					+ "La fecha de emisión de la factura es anterior a la fecha de finalización del periodo de facturación.");
-			
-		this.codigo = codigo;
-		this.tarifa = tarifa;
-		this.fechaEmision = fechaEmision;
-		this.periodoInicio = periodoInicio;
-		this.periodoFinal = periodoFinal;
-		this.importe = importe;
+		data = new HashMap<>();
 	}
 	
-	public void calcularImporte(Cliente cliente, DatosLlamadas llamadas)
-			throws IllegalArgumentException, NoSuchElementException{
+	public void añadirFactura(Cliente cliente, Factura factura) throws IllegalArgumentException{
 		
-		this.tarifa = cliente.getTarifa();
+		if(!data.containsKey(cliente))
+			data.put(cliente, new ArrayList<>());
 		
-		ArrayList<LocalTime> llamadasRango;
-		
-		try{
-			llamadasRango = llamadas.buscarLlamadaPeriodo(cliente, periodoInicio, periodoFinal);
-		}catch(IllegalArgumentException e){
-			throw new IllegalArgumentException("Periodo de busqueda invalido. "
-					+ "La fecha de comienzo del periodo no puede ser mayor o igual a la del fin.");
-		}catch(NoSuchElementException e){
-			throw new NoSuchElementException("El cliente no tiene ninguna llamada en la base de datos. No se puede calcular el importe");
-		}
-		
-		if(!llamadasRango.isEmpty()){
-			double total = 0;
-			
-			for(int i = 0; i < llamadasRango.size(); i++){
-				
-				total += timeToMin(llamadasRango.get(i));
-			}
-			importe = total * cliente.getTarifa().toDouble();
-		
-		}else{
-			
-			importe = 0;
-		}
+		if(!data.get(cliente).contains(factura))
+			data.get(cliente).add(factura);
+		else
+			throw new IllegalArgumentException("Ya existe esa factura en la base de datos.");
 	}
 	
-	private double timeToMin(LocalTime tiempo){
+	public ArrayList<Factura> facturasCliente(Cliente cliente) throws NoSuchElementException{
+		if(!data.containsKey(cliente))
+			throw new NoSuchElementException("El cliente no tiene ninguna llamada en la base de datos.");
 		
-		double min = tiempo.getHour()* 60 + tiempo.getMinute() + tiempo.getSecond() / 60.0;
-		
-		return min;
-	}
-	
-	public int getCodigo(){
-		
-		return codigo;
-	}
-	
-	public Tarifa getTarifa(){
-		
-		return tarifa;
-	}
-	
-	public LocalDate getFecha(){
-		
-		return fechaEmision;
-	}
-	
-	public LocalDateTime getPeriodoInicio(){
-		
-		return periodoInicio;
-	}
-	
-	public LocalDateTime getPeriodoFinal(){
-	
-		return periodoFinal;
-	}
-	
-	public double getImporte(){
-		
-		return importe;
+		return data.get(cliente);
 	}
 }
