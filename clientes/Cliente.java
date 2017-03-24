0de1fc2899389176e5b76a477d510d1284b45045
byUNiXx <clmjuanluisc@gmail.com>
2017-03-24 13:08:53 +0100

    Update Cliente.java

--- clientes/Cliente.java
+++ clientes/Cliente.java
@@ -1,8 +1,6 @@
 package cliente;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 
 import tarifa.Tarifa;
 
@@ -15,8 +13,6 @@ public class Cliente{
     private LocalDate fechaAlta;
     private Tarifa tarifa;
     
-    public Cliente(){
-    }
 
     public Cliente(String NIF, String nombre, String correoElectronico, Direccion direccion, 
     		LocalDate fecha, Tarifa tarifa) {
@@ -60,21 +56,5 @@ public class Cliente{
     	
     	return NIF + "  " +  nombre + "  " + correoElectronico + "  " + direccion 
     			+ "  " + fechaAlta + "  " + tarifa;
-    }
-    
-    public static void main(String[] args) {
-    	
-    	LocalDate fecha = LocalDate.of(2017, 11, 23);
-    	
-    	LocalTime hora = LocalTime.now();
-    	
-    	Cliente cliente = new Particular("53788220Y","Diego","Cano","al342376@uji.es",
-    			new Direccion("12540","Vila-real","Castellón"),fecha , new Tarifa(0.37));
-    	
-    	/*Cliente cliente2 = new Empresa("53788220Y","Diego","al342376@uji.es",
-    			new Direccion("12540","Vila-real","Castellón"), new Fecha(25, 01, 2017), new Tarifa(0.37));*/
-        
-    	System.out.println(cliente);
-    	System.out.println(hora.truncatedTo(ChronoUnit.SECONDS)); //No hagas caso a esto, solo estaba probando cosas.
     }
 }
