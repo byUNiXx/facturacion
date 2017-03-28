8f27db5bff22caeb8cf33cc62b184b33dcc10c8e
byUNiXx <clmjuanluisc@gmail.com>
2017-03-28 19:19:43 +0200

    Update Init.java

--- main/Init.java
+++ main/Init.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 import datos.DatosClientes;
 import datos.DatosFacturas;
 import datos.DatosLlamadas;
@@ -11,11 +14,43 @@ public class Init {
 	public static DatosLlamadas dataLlamadas;
 	public static DatosFacturas dataFacturas;
 
-	public static void init(){
+	public static void preInit(){
 		 
 		dataClientes = new DatosClientes();
 		dataLlamadas = new DatosLlamadas();
 		dataFacturas = new DatosFacturas();
-		MenuPrincipal.inicio();
+		
+		init();
+	}
+	
+	private static void init(){
+		
+		
+		try{
+			FileInputStream fichero = new FileInputStream("datosClientes.bin");
+			ObjectInputStream carga = new ObjectInputStream(fichero);
+			
+			dataClientes = (DatosClientes)carga.readObject();
+			
+			fichero = new FileInputStream("datosLlamadas.bin");
+			carga = new ObjectInputStream(fichero);
+			
+			dataLlamadas = (DatosLlamadas)carga.readObject();
+			
+			fichero = new FileInputStream("datosFacturas.bin");
+			carga = new ObjectInputStream(fichero);
+			
+			dataFacturas = (DatosFacturas)carga.readObject();
+			
+			carga.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}finally{
+			
+			MenuPrincipal.inicio();
+		}
+		
 	}
 }
